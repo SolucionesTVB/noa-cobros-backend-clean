a3f24e706b090cae44c6dbac35340e8fae6f7ac4
SolucionesTVB <vtonyb@gmail.com>
2025-09-25 15:10:02 -0600

    main

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 # wsgi.py — entrada para gunicorn (Render)
 try:
     from app import app as application  # gunicorn usa "application"
-    app = application                   # alias por si usan wsgi:app
+    app = application # alias por si usan wsgi:app
 except Exception as e:
     raise RuntimeError(f"WSGI import error desde app.py: {e}")
